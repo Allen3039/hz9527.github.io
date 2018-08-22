91f80d6a03373c76f9d16a0830398dd1f8a96d6a
hz9527 <981379316@qq.com>
2018-08-22 08:25:32 +0800

    change some file

--- app/build/update.sh
+++ app/build/update.sh
@@ -4,12 +4,12 @@ if [[ $REPLY =~ ^[Yy]$ ]];then
   cd ..
   br=`git branch | grep "*"`
   if [ "master"==${br/* /} ];then
+    node ./app/build/build.js
     git status
     git add .
     git commit -m ":memo: update blog site"
     git pull origin master
     if [ 0==$? ];then
-      node ./app/build/build.js
       git push origin master
       cd app
       echo -e "\033[42;37m update successful \033[0m"
