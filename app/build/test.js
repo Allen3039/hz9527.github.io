53d99398d45b897b3d26d8c71d294d62f3051ac2
hz9527 <981379316@qq.com>
2018-08-22 08:15:31 +0800

    :memo: update blog site

--- app/build/test.js
+++
@@ -1,8 +0,0 @@
-// 生成markdown配置文件
-// const generator = require('./generator.js')
-
-// generator(process.env.NODE_ENV === 'production' ? 'build' : 'dev')
-
-const tool = require('./mdTool.js')
-
-tool('build')
