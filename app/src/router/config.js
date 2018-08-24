78322c1f63716ef3e69fe54d8e8a15d221aa337f
hz9527 <981379316@qq.com>
2018-08-24 11:02:48 +0800

    add

--- app/src/router/config.js
+++ app/src/router/config.js
@@ -1,5 +1,6 @@
 export default [
 	{title: '简单的中间件', type: 'js', tips: ['middleware', '中间件', '中间件实现'], time: 1528909418611, file: 'middleware.md'},
 	{title: '实现一个promise', type: 'js', tips: ['promise', 'promise实现'], time: 1529199994117, file: 'promise_polyfill.md'},
-	{title: 'serviceWorker系列一', type: 'js', tips: ['serviceWorker', 'sw基础'], time: 1529302280810, file: 'serviceWorker1.md'}
+	{title: 'serviceWorker系列一', type: 'js', tips: ['serviceWorker', 'sw基础'], time: 1529302280810, file: 'serviceWorker1.md'},
+	{title: 'async&await正确的打开方式', type: 'js', tips: ['async', 'await', 'async&await使用'], time: 1535007612399, file: 'async&await.md'}
 ]
