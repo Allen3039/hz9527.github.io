3cb2d2d3c886f16cc53628d9180a5cc13f04c256
hz9527 <981379316@qq.com>
2018-11-09 00:09:48 +0800

    :memo: update blog site

--- app/src/router/config.js
+++ app/src/router/config.js
@@ -1,9 +1,7 @@
 export default [
 	{title: 'async&await正确的打开方式', type: 'js', tips: ['async', 'await', 'async&await使用'], time: 1535007612399, file: 'async&await.md', show: false},
-	{title: 'call、apply、bind简单实现', type: 'js', tips: ['call实现', 'apply实现', 'bind实现'], time: 1541634698160, file: 'call.md', show: true},
+	{title: 'call、apply、bind简单实现', type: 'js', tips: ['call实现', 'apply实现', 'bind实现'], time: 1541691883074, file: 'call.md', show: true},
 	{title: '简单的中间件', type: 'js', tips: ['middleware', '中间件', '中间件实现'], time: 1528909418611, file: 'middleware.md', show: false},
 	{title: '实现一个promise', type: 'js', tips: ['promise', 'promise实现'], time: 1529199994117, file: 'promise_polyfill.md', show: false},
-	{title: 'serviceWorker系列一', type: 'js', tips: ['serviceWorker', 'sw基础'], time: 1529302280810, file: 'serviceWorker1.md', show: false},
-	{title: 'call、apply、bind简单实现', type: 'js', tips: ['call实现', 'apply实现', 'bind实现'], time: 1541691883074, file: 'call.md', show: true},
-	{title: 'call、apply、bind简单实现', type: 'js', tips: ['call实现', 'apply实现', 'bind实现'], time: 1541691883074, file: 'call.md', show: true}
+	{title: 'serviceWorker系列一', type: 'js', tips: ['serviceWorker', 'sw基础'], time: 1529302280810, file: 'serviceWorker1.md', show: false}
 ]
